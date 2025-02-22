ab1d0d6888d0929832094e3e0590d6c42c39630c
arturz777 <margo310507@gmail.com>
2025-02-23 00:34:15 +0200

    Update warehouseAPI.js

--- client/src/http/warehouseAPI.js
+++ client/src/http/warehouseAPI.js
@@ -1,16 +1,16 @@
 import { $authHost } from "./index";
 
 export const fetchWarehouseOrders = async () => {
-  const { data } = await $authHost.get("/api/warehouse/orders");
+  const { data } = await $authHost.get("/warehouse/orders");
   return data;
 };
 
 export const acceptWarehouseOrder = async (orderId, processingTime) => {
-  const { data } = await $authHost.post(`/api/warehouse/orders/${orderId}/accept`, { processingTime });
+  const { data } = await $authHost.post(`/warehouse/orders/${orderId}/accept`, { processingTime });
   return data;
 };
 
 export const completeWarehouseOrder = async (orderId) => {
-  const { data } = await $authHost.post(`/api/warehouse/orders/${orderId}/complete`);
+  const { data } = await $authHost.post(`/warehouse/orders/${orderId}/complete`);
   return data;
 };
