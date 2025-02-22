0a578dc9a6b3f41a1645a0fcc68e9ad60aa4eb0d
arturz777 <margo310507@gmail.com>
2025-02-22 14:16:06 +0200

    http/index.js

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $host = axios.create({ 
-	baseURL: process.env.REACT_APP_API_URL
+	baseURL: process.env.REACT_APP_API_URL || "https://zang-4.onrender.com/api"
 })
 
 const $authHost = axios.create({
-     baseURL: process.env.REACT_APP_API_URL
+     baseURL: process.env.REACT_APP_API_URL || "https://zang-4.onrender.com/api"
 })
 
 const authInterceptor = config => { 
