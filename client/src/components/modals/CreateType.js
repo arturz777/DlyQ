de55fd14e08bc60e3024ef510628c98ba63bc793
arturz777 <margo310507@gmail.com>
2025-02-26 22:22:30 +0200

    Update CreateType.js

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -6,18 +6,27 @@ import { createType, updateType } from "../../http/deviceAPI";
 const CreateType = ({ show, onHide, editableType, onTypeSaved }) => {
   const [value, setValue] = useState("");
   const [file, setFile] = useState(null);
+  const [existingImage, setExistingImage] = useState(null);
   const [errors, setErrors] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   useEffect(() => {
     if (editableType) {
-        setValue(editableType.name);
-        setFile(null); // Или оставить старое изображение
+      setValue(editableType.name);
+      setExistingImage(editableType.img);
+      setFile(null);
     } else {
-        setValue("");
-        setFile(null);
+      resetFields();
     }
-}, [editableType]);
+  }, [editableType]);
+
+  const resetFields = () => {
+    setValue("");
+    setFile(null);
+    setExistingImage(null);
+    setErrors({});
+    setIsSubmitted(false);
+  };
 
   const selectFile = (e) => {
     setFile(e.target.files[0]);
@@ -31,82 +40,82 @@ const CreateType = ({ show, onHide, editableType, onTypeSaved }) => {
       return;
     }
 
+    if (!file && !existingImage) {
+      setErrors({ img: "Загрузите изображение" });
+      return;
+    }
+
     const formData = new FormData();
     formData.append("name", value);
     if (file) formData.append("img", file);
 
-    if (editableType) {
-      // Если редактируем тип
-      updateType(editableType.id, formData)
-        .then((data) => {
-          setErrors({});
-          setIsSubmitted(false);
-          setValue("");
-          setFile(null);
-          onHide();
-          onTypeSaved(); // Оповещаем о сохранении
-      })
-      .catch((err) => {
-        setIsSubmitted(false);
-      });
-  } else {
-    // Если создаем новый тип
-    createType(formData)
-      .then((data) => {
-        setErrors({});
-        setIsSubmitted(false);
-        setValue("");
-        setFile(null);
+    const saveAction = editableType
+      ? updateType(editableType.id, formData)
+      : createType(formData);
+
+    saveAction
+      .then(() => {
         onHide();
+        onTypeSaved();
+        resetFields();
       })
       .catch((err) => {
-        setIsSubmitted(false);
+        console.error("Ошибка при сохранении типа:", err);
       });
-    }
-    
   };
 
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
-    <Modal.Header closeButton>
-    <Modal.Title id="contained-modal-title-vcenter">
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
           {editableType ? "Редактировать тип" : "Добавить тип"}
         </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <Form>
-        <Form.Control
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="Введите название типа"
-        />
-        {isSubmitted && !value && (
-          <span style={{ color: "red", display: "block", marginTop: "5px" }}>
-            Введите название типа
-          </span>
-        )}
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Control
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Введите название типа"
+          />
+          {isSubmitted && !value && (
+            <span style={{ color: "red", display: "block", marginTop: "5px" }}>
+              Введите название типа
+            </span>
+          )}
 
-        <Form.Control
-          className="mt-3"
-          type="file"
-          onChange={selectFile}
-        />
-        {isSubmitted && !file && (
-          <span style={{ color: "red", display: "block", marginTop: "5px" }}>
-            Загрузите изображение
-          </span>
-        )}
-      </Form>
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="outline-danger" onClick={onHide}>
-        Закрыть
-      </Button>
-      <Button onClick={addType}>
+          <Form.Control type="file" onChange={selectFile} />
+          {existingImage && !file && (
+            <div className="mt-2">
+               <img
+                  src={existingImage}
+                  alt="Текущее изображение"
+                  style={{
+                    width: "100px",
+                    borderRadius: "5px",
+                    objectFit: "cover",
+                    border: "1px solid #ddd",
+                    padding: "5px",
+                  }}
+                />
+            </div>
+          )}
+          {isSubmitted && !file && (
+            <span style={{ color: "red", display: "block", marginTop: "5px" }}>
+              Загрузите изображение
+            </span>
+          )}
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="outline-danger" onClick={onHide}>
+          Закрыть
+        </Button>
+        <Button onClick={addType}>
           {editableType ? "Сохранить изменения" : "Добавить"}
         </Button>
-    </Modal.Footer>
-  </Modal>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
