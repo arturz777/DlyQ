933fb5b794a4692bcc351075f09ba72b4b152ec7
arturz777 <margo310507@gmail.com>
2025-02-28 21:38:45 +0200

    Update Courier.js

--- client/src/pages/Courier.js
+++ client/src/pages/Courier.js
@@ -18,7 +18,8 @@ import { io } from "socket.io-client";
 import { completeDelivery, updateDeliveryStatus, updateCourierLocation  } from "../http/courierAPI";
 import styles from "./Courier.module.css";
 
-const socket = io("https://zang-4.onrender.com");
+const socket = io(process.env.REACT_APP_API_URL || "https://zang-4.onrender.com");
+
 
 const WAREHOUSE_LOCATION = { lat: 59.513720, lng: 24.828888 };
  
