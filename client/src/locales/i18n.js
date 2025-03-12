43b91acb4b9671a6545fce28bad6397fe2e34b03
arturz777 <margo310507@gmail.com>
2025-03-12 17:36:23 +0200

    Update i18n.js

--- client/src/locales/i18n.js
+++ client/src/locales/i18n.js
@@ -9,18 +9,21 @@ import searchbarEst from './est/searchbar.json';
 import devicePageEst from './est/devicePage.json';
 import deviceItemEst from './est/deviceItem.json';
 import deviceListEst from './est/deviceList.json';
+import cookiePolicyEst from './est/cookiePolicy.json';
 
 import navbarEn from './en/navbar.json';
 import searchbarEn from './en/searchbar.json';
 import devicePageEn from './en/devicePage.json';
 import deviceItemEn from './en/deviceItem.json';
 import deviceListEn from './en/deviceList.json';
+import cookiePolicyEn from './en/cookiePolicy.json';
 
 import navbarRu from './ru/navbar.json';
 import searchbarRu from './ru/searchbar.json';
 import devicePageRu from './ru/devicePage.json';
 import deviceItemRu from './ru/deviceItem.json';
 import deviceListRu from './ru/deviceList.json';
+import cookiePolicyRu from './ru/cookiePolicy.json';
 
 
 i18n
@@ -34,6 +37,7 @@ i18n
         devicePage: devicePageEn,
         deviceItem: deviceItemEn,
         deviceList: deviceListEn,
+        cookiePolicy: cookiePolicyEn,
       },
       ru: {
         navbar: navbarRu,
@@ -41,6 +45,7 @@ i18n
         devicePage: devicePageRu,
         deviceItem: deviceItemRu,
         deviceList: deviceListRu,
+        cookiePolicy: cookiePolicyRu,
       },
       est: {
         navbar: navbarEst,
@@ -48,6 +53,7 @@ i18n
         devicePage: devicePageEst,
         deviceItem: deviceItemEst,
         deviceList: deviceListEst,
+        cookiePolicy: cookiePolicyEst,
       },
     },
 
