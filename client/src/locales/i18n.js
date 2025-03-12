9c43b9154679d89b1e59e7a843c3e72ab9258da6
arturz777 <margo310507@gmail.com>
2025-03-12 16:06:11 +0200

    Update i18n.js

--- client/src/locales/i18n.js
+++ client/src/locales/i18n.js
@@ -6,13 +6,21 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 // Импорты переводов
 import navbarEst from './est/navbar.json';
 import searchbarEst from './est/searchbar.json';
+import devicePageEst from './est/devicePage.json';
+import deviceItemEst from './est/deviceItem.json';
+import deviceListEst from './est/deviceList.json';
 
 import navbarEn from './en/navbar.json';
 import searchbarEn from './en/searchbar.json';
+import devicePageEn from './en/devicePage.json';
+import deviceItemEn from './en/deviceItem.json';
+import deviceListEn from './en/deviceList.json';
 
 import navbarRu from './ru/navbar.json';
 import searchbarRu from './ru/searchbar.json';
-
+import devicePageRu from './ru/devicePage.json';
+import deviceItemRu from './ru/deviceItem.json';
+import deviceListRu from './ru/deviceList.json';
 
 
 i18n
@@ -23,14 +31,23 @@ i18n
       en: {
         navbar: navbarEn,
         searchbar: searchbarEn,
+        devicePage: devicePageEn,
+        deviceItem: deviceItemEn,
+        deviceList: deviceListEn,
       },
       ru: {
         navbar: navbarRu,
         searchbar: searchbarRu,
+        devicePage: devicePageRu,
+        deviceItem: deviceItemRu,
+        deviceList: deviceListRu,
       },
       est: {
         navbar: navbarEst,
         searchbar: searchbarEst,
+        devicePage: devicePageEst,
+        deviceItem: deviceItemEst,
+        deviceList: deviceListEst,
       },
     },
 
