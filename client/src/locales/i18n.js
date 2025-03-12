262ba1bd286637f29dba9a1e1583074a7d7703ea
arturz777 <margo310507@gmail.com>
2025-03-12 23:05:57 +0200

    Update i18n.js

--- client/src/locales/i18n.js
+++ client/src/locales/i18n.js
@@ -1,15 +1,15 @@
-// client/src/locales/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Импорты переводов
+
 import navbarEst from './est/navbar.json';
 import searchbarEst from './est/searchbar.json';
 import devicePageEst from './est/devicePage.json';
 import deviceItemEst from './est/deviceItem.json';
 import deviceListEst from './est/deviceList.json';
 import cookiePolicyEst from './est/cookiePolicy.json';
+import returnPolicyEst from './est/returnPolicy.json';
 
 import navbarEn from './en/navbar.json';
 import searchbarEn from './en/searchbar.json';
@@ -17,6 +17,7 @@ import devicePageEn from './en/devicePage.json';
 import deviceItemEn from './en/deviceItem.json';
 import deviceListEn from './en/deviceList.json';
 import cookiePolicyEn from './en/cookiePolicy.json';
+import returnPolicyEn from './en/returnPolicy.json';
 
 import navbarRu from './ru/navbar.json';
 import searchbarRu from './ru/searchbar.json';
@@ -24,11 +25,12 @@ import devicePageRu from './ru/devicePage.json';
 import deviceItemRu from './ru/deviceItem.json';
 import deviceListRu from './ru/deviceList.json';
 import cookiePolicyRu from './ru/cookiePolicy.json';
+import returnPolicyRu from './ru/returnPolicy.json';
 
 
 i18n
-  .use(LanguageDetector) // Автоматическое определение языка
-  .use(initReactI18next) // Интеграция с React
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
     resources: {
       en: {
@@ -38,6 +40,7 @@ i18n
         deviceItem: deviceItemEn,
         deviceList: deviceListEn,
         cookiePolicy: cookiePolicyEn,
+        returnPolicy: returnPolicyEn,
       },
       ru: {
         navbar: navbarRu,
@@ -46,6 +49,7 @@ i18n
         deviceItem: deviceItemRu,
         deviceList: deviceListRu,
         cookiePolicy: cookiePolicyRu,
+        returnPolicy: returnPolicyRu,
       },
       est: {
         navbar: navbarEst,
@@ -54,6 +58,7 @@ i18n
         deviceItem: deviceItemEst,
         deviceList: deviceListEst,
         cookiePolicy: cookiePolicyEst,
+        returnPolicy: returnPolicyEst,
       },
     },
 
