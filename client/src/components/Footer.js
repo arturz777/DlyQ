e6db2001e018dd2bd51a477de3c7db8789d44cde
arturz777 <margo310507@gmail.com>
2025-03-13 14:44:05 +0200

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Footer.module.css";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const { t, i18n } = useTranslation();
+    
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         {/* Логотип и копирайт */}
         <div className={styles.logoSection}>
-          <h2>MyCompany</h2>
-          <p>© 2024 MyCompany. Все права защищены.</p>
+          <h2>Zang</h2>
+          <p>© 2024 Zang. {t("all rights reserved", { ns: "footer" })}</p>
         </div>
 
         {/* Полезные ссылки */}
         <div className={styles.linksSection}>
-        <Link to="/terms-of-service">Пользовательское соглашение</Link>
-        <Link to="/privacy-policy">Политика конфиденциальности</Link>
-        <Link to="/return-policy">Гарантия и возврат</Link>
-        <Link to="/shipping-policy">Доставка</Link>
+        <Link to="/terms-of-service">{t("user Agreement", { ns: "footer" })}</Link>
+        <Link to="/privacy-policy">{t("privacy Policy", { ns: "footer" })}</Link>
+        <Link to="/return-policy">{t("warranty and Returns", { ns: "footer" })}</Link>
+        <Link to="/shipping-policy">{t("delivery", { ns: "footer" })}</Link>
         <Link to="/cookie-policy">Cookie</Link>
         </div>
 
