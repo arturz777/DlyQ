fba4988f35722a43579bf3e599c5b168af22f155
arturz777 <margo310507@gmail.com>
2025-03-16 14:23:52 +0200

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,6 +1,7 @@
 // server/controllers/orderController.js
 const sendEmail = require("../services/emailService");
-const { Order, Device } = require("../models/models");
+const { Order, Device, , Translation } = require("../models/models");
+const { Op } = require("sequelize");
 const getDistanceFromWarehouse = require("../utils/distance");
 const { supabase } = require("../config/supabaseClient"); // ✅ Импорт Supabase
 const uuid = require("uuid");
