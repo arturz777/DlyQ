9722eda96b2993518a08101b644441c4afa49b50
arturz777 <margo310507@gmail.com>
2025-03-17 18:37:15 +0200

    Update TypeBar.js

--- client/src/components/TypeBar.js
+++ client/src/components/TypeBar.js
@@ -1,35 +1,39 @@
-import React, {useContext} from 'react';
-import {observer} from "mobx-react-lite";
-import {Context} from "../index";
+import React, { useContext } from "react";
+import { observer } from "mobx-react-lite";
+import { Context } from "../index";
+import { useTranslation } from "react-i18next";
 import styles from "./TypeBar.module.css";
 
 const TypeBar = observer(() => {
-	const {device} = useContext(Context)
+  const { device } = useContext(Context);
+  const { i18n } = useTranslation();
+  const currentLang = i18n.language || "en";
 
-
-	return (
-		<div className={styles.typeBar}>
-		{device.types.map(type => (
-			<div
-				key={type.id}
-				id={`type-${type.id}`}
-				className={`${styles.typeItem} ${type.id === device.selectedType.id ? styles.active : ''}`}
-				onClick={() => {
-					device.setSelectedType(type); // Устанавливаем выбранный тип
-					document.getElementById(`type-${type.id}`).scrollIntoView({
-						behavior: "smooth", // Плавная прокрутка
-						block: "start",
-					  });
-				}}
-			>
-			<img src={type.img} alt={type.name} className={styles.typeImage} />
-			<span className={styles.typeName}>{type.name}</span>
-			</div>
-		))}
-	</div>
-
-
-	);
+  return (
+    <div className={styles.typeBar}>
+      {device.types.map((type) => (
+        <div
+          key={type.id}
+          id={`type-${type.id}`}
+          className={`${styles.typeItem} ${
+            type.id === device.selectedType.id ? styles.active : ""
+          }`}
+          onClick={() => {
+            device.setSelectedType(type);
+            document.getElementById(`type-${type.id}`).scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
+          }}
+        >
+          <img src={type.img} alt={type.name} className={styles.typeImage} />
+          <span className={styles.typeName}>
+            {type.translations?.name?.[currentLang] || type.name}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
 });
 
 export default TypeBar;
