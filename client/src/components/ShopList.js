6d091c94f7142490a2c683877939c1c68598b944
arturz777 <margo310507@gmail.com>
2025-03-17 18:41:36 +0200

    Update ShopList.js

--- client/src/components/ShopList.js
+++ client/src/components/ShopList.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import DeviceItem from "./DeviceItem";
+import { useTranslation } from "react-i18next";
 import styles from "./DeviceList.module.css"; // Подключаем стили из DeviceList
 
 const ShopList = ({ devices, types, subtypes }) => {
+  const { i18n } = useTranslation();
+  const currentLang = i18n.language || "en";
   const [groupedDevices, setGroupedDevices] = useState({});
 
   useEffect(() => {
     // Группируем устройства по типам и подтипам
     const grouped = types.reduce((acc, type) => {
       acc[type.id] = {
-        typeName: type.name,
+        typeName: type.translations?.name?.[currentLang] || type.name,
         subtypes: {},
         noSubtypeDevices: [], // Устройства без подтипа
       };
@@ -20,7 +23,7 @@ const ShopList = ({ devices, types, subtypes }) => {
         .forEach((sub) => {
           acc[type.id].subtypes[sub.id] = {
             devices: [],
-            subtypeName: sub.name,
+            subtypeName: sub.translations?.name?.[currentLang] || sub.name, 
           };
         });
 
@@ -39,7 +42,7 @@ const ShopList = ({ devices, types, subtypes }) => {
     }, {});
 
     setGroupedDevices(grouped);
-  }, [devices, types, subtypes]);
+  }, [devices, types, subtypes, currentLang]);
 
   return (
     <div>
@@ -48,10 +51,10 @@ const ShopList = ({ devices, types, subtypes }) => {
 
         return (
           <div key={typeId} className={styles.section}>
-            {/* Заголовок типа */}
+         
             <h2 className={styles.sectionTitle}>{typeGroup.typeName}</h2>
 
-            {/* Устройства без подтипов */}
+          
             {typeGroup.noSubtypeDevices.length > 0 && (
               <div className={styles.deviceGrid}>
                 {typeGroup.noSubtypeDevices.map((device) => (
@@ -60,7 +63,7 @@ const ShopList = ({ devices, types, subtypes }) => {
               </div>
             )}
 
-            {/* Устройства с подтипами */}
+          
             {Object.keys(typeGroup.subtypes).map((subtypeId) => {
               const subtypeGroup = typeGroup.subtypes[subtypeId];
 
@@ -70,7 +73,7 @@ const ShopList = ({ devices, types, subtypes }) => {
 
               return (
                 <div key={subtypeId} className={styles.subtypeSection}>
-                  {/* Заголовок подтипа */}
+               
                   <h3 className={styles.subtypeTitle}>
                     {subtypeGroup.subtypeName}
                   </h3>
