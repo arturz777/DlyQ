bdd9c8fd4ccd20ea0e96115918592a3c333d8adb
arturz777 <margo310507@gmail.com>
2025-03-21 18:51:58 +0200

    Update consts.js

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -2,7 +2,7 @@
 export const ADMIN_ROUTE = '/admin';
 export const LOGIN_ROUTE = '/login';
 export const REGISTRATION_ROUTE = '/registration';
-export const SHOP_ROUTE = '/';
+export const SHOP_ROUTE = '/shop';
 export const BASKET_ROUTE = '/basket';
 export const DEVICE_ROUTE = '/device';
 export const TERMS_ROUTE = '/terms-of-service';
