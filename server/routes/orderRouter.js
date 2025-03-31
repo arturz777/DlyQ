4c6c60d605ef93794456af73dffcbd7b569ccc42
arturz777 <margo310507@gmail.com>
2025-04-01 01:47:31 +0300

    Update orderRouter.js

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -1,8 +1,8 @@
-// server/routes/orderRoutes.js
 const Router = require('express');
 const router = new Router();
 const orderController = require('../controllers/orderController');
 const authMiddleware = require('../middleware/authMiddleware');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
 
 router.post('/create', authMiddleware, orderController.createOrder);
@@ -10,6 +10,11 @@ router.get('/user', authMiddleware, orderController.getUserOrders);
 router.put('/update-status', authMiddleware, orderController.updateOrderStatus);
 router.get('/active', authMiddleware, orderController.getActiveOrder);
 router.get("/delivery-cost", orderController.getDeliveryCost);
+router.get('/admin', authMiddleware, checkRole("ADMIN"), orderController.getAllOrdersForAdmin);
+router.put('/:id/status', authMiddleware, checkRole("ADMIN"), orderController.adminUpdateOrderStatus);
+router.put('/:id/assign-courier', authMiddleware, checkRole("ADMIN"), orderController.assignCourier);
+
+
 
 
 module.exports = router;
