79debb2a7cd733b22ec6a376c5ca11125eb4f392
arturz777 <margo310507@gmail.com>
2025-04-03 00:38:39 +0300

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,16 +9,15 @@ import {
 } from "../utils/consts";
 import { observer } from "mobx-react-lite";
 import { useNavigate } from "react-router-dom";
-import SearchBar from "./SearchBar"; // Импортируем SearchBar
+import SearchBar from "./SearchBar";
 import styles from "./NavBar.module.css";
-import Flag from "react-world-flags";
 import {
   FaShoppingCart,
   FaCog,
   FaListAlt,
   FaUser,
   FaSignOutAlt,
-} from "react-icons/fa"; // Для иконок
+} from "react-icons/fa";
 import { useTranslation } from "react-i18next";
 
 const NavBar = observer(() => {
@@ -37,18 +36,18 @@ const NavBar = observer(() => {
   ];
 
   const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng); // Смена языка
+    i18n.changeLanguage(lng);
     setIsLanguageMenuOpen(false);
   };
 
-  const currentLanguage = i18n.language; // Текущий язык
+  const currentLanguage = i18n.language;
   const currentFlag = languages.find(
     (lang) => lang.language === currentLanguage
   );
 
   const handleLogOut = () => {
-    logOut(); // Выход пользователя
-    navigate("/login"); // Возврат на главную страницу
+    logOut();
+    navigate("/login");
   };
 
   useEffect(() => {
@@ -68,16 +67,16 @@ const NavBar = observer(() => {
 
   const navbarStyle = {
     position: "fixed",
-    top: scrollDirection === "up" ? "0" : "-150px", // Скрыть при прокрутке вниз
+    top: scrollDirection === "up" ? "0" : "-150px",
     width: "100%",
     transition: "top 0.3s",
     zIndex: 1000,
   };
 
   const logOut = () => {
-    localStorage.removeItem("token"); // Удаляем токен из локального хранилища
-    user.setUser({}); // Сбрасываем пользователя
-    user.setIsAuth(false); // Устанавливаем, что пользователь не авторизован
+    localStorage.removeItem("token");
+    user.setUser({});
+    user.setIsAuth(false);
   };
 
   return (
@@ -87,24 +86,26 @@ const NavBar = observer(() => {
           Zang
         </NavLink>
 
-        {/* Иконка "Каталог" */}
         <NavLink to="/catalog" className={styles.navbarLink}>
           <FaListAlt />
           <span className={styles.navbarLinkTitle}>{t("catalog")}</span>
         </NavLink>
 
-        {/* Поиск */}
         <SearchBar />
 
         <div
           className={styles.languageSelectorWrapper}
-          onMouseLeave={() => setIsLanguageMenuOpen(false)} // Закрыть меню при уходе курсора
+          onMouseLeave={() => setIsLanguageMenuOpen(false)} 
         >
           <button
             onClick={() => setIsLanguageMenuOpen(!isLanguageMenuOpen)}
             className={styles.currentLanguageButton}
           >
-            <Flag code={currentFlag?.code} className={styles.flag} />
+            <img
+              src={require(`../assets/flags/${currentFlag?.language}.png`)}
+              alt={currentFlag?.language}
+              className={styles.flag}
+            />
           </button>
           {isLanguageMenuOpen && (
             <div className={styles.dropdownMenu}>
@@ -114,7 +115,11 @@ const NavBar = observer(() => {
                   onClick={() => changeLanguage(lang.language)}
                   className={styles.dropdownItem}
                 >
-                  <Flag code={lang.code} className={styles.flag} />
+                  <img
+                    src={require(`../assets/flags/${lang.language}.png`)}
+                    alt={lang.language}
+                    className={styles.flag}
+                  />
                 </button>
               ))}
             </div>
