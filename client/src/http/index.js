85511963c465eb95d04be44168743e6a7ebe9bc3
arturz777 <margo310507@gmail.com>
2025-04-15 13:17:51 +0300

    Update index.js

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,22 +1,96 @@
 import axios from "axios";
 
 const $host = axios.create({ 
-	baseURL: process.env.REACT_APP_API_URL
-})
-
+	baseURL: process.env.REACT_APP_API_URL,
+	withCredentials: true
+});
 
 const $authHost = axios.create({
-     baseURL: process.env.REACT_APP_API_URL
-})
+	baseURL: process.env.REACT_APP_API_URL,
+	withCredentials: true
+});
 
 const authInterceptor = config => { 
-	config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
-	return config 
-}
+	const token = localStorage.getItem('token');
+	if (token) {
+		config.headers.authorization = `Bearer ${token}`;
+	}
+	return config;
+};
 
-$authHost.interceptors.request.use(authInterceptor)
+$authHost.interceptors.request.use(authInterceptor);
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+$authHost.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/user/login") &&
+      !originalRequest.url.includes("/user/registration")
+    ) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        }).then((token) => {
+          originalRequest.headers.authorization = `Bearer ${token}`;
+          return $authHost(originalRequest);
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      try {
+        const response = await $host.post(
+          "/user/refresh",
+          {},
+          { withCredentials: true }
+        );
+
+        const newToken = response.data.accessToken;
+        localStorage.setItem("token", newToken);
+
+        $authHost.defaults.headers.authorization = `Bearer ${newToken}`;
+        processQueue(null, newToken);
+
+        originalRequest.headers.authorization = `Bearer ${newToken}`;
+        return $authHost(originalRequest);
+      } catch (err) {
+        processQueue(err, null);
+        localStorage.removeItem("token");
+        return Promise.resolve({ data: null });
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    if (error.response?.status === 401) {
+      return Promise.resolve({ data: null });
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 
 export {
 	$host,
 	$authHost
-}
+};
