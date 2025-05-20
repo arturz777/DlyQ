26e051cfa2e1a5f27412971b875d7b9b33301488
arturz777 <margo310507@gmail.com>
2025-05-20 22:18:33 +0300

    Update MobileNavBar.js

--- client/src/components/MobileNavBar.js
+++ client/src/components/MobileNavBar.js
@@ -1,17 +1,18 @@
 import React, { useContext, useEffect  } from "react";
 import { Context } from "../index";
 import { useNavigate, useLocation  } from "react-router-dom";
+import { observer } from "mobx-react-lite";
 import { useTranslation } from "react-i18next";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const MobileNavBar = () => {
   const navigate = useNavigate();
-  const { user } = useContext(Context);
+  const { user, basket } = useContext(Context);
   const location = useLocation();
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
-    window.scrollTo(0, 0); // Прокручиваем страницу вверх
+    window.scrollTo(0, 0); 
   }, [location.pathname]);
 
   return (
@@ -33,14 +34,34 @@ const MobileNavBar = () => {
           <i className="bi bi-search" style={{ fontSize: "1.5rem" }}></i>
           <div style={{ fontSize: "0.75rem" }}>{t("search", { ns: "mobileNavBar" })}</div>
         </div>
-        <div
-          className="text-center"
-          onClick={() => navigate("/basket")}
-          style={{ cursor: "pointer" }}
-        >
-          <i className="bi bi-bag" style={{ fontSize: "1.5rem" }}></i>
-          <div style={{ fontSize: "0.75rem" }}>{t("cart", { ns: "mobileNavBar" })}</div>
-        </div>
+       <div
+  className="text-center position-relative"
+  onClick={() => navigate("/basket")}
+  style={{ cursor: "pointer" }}
+>
+  <i className="bi bi-bag" style={{ fontSize: "1.5rem", position: "relative" }}></i>
+  
+  {basket.totalItems > 0 && (
+    <span
+      style={{
+        position: "absolute",
+        top: "0px",
+        right: "-6px",
+        backgroundColor: "#dc3545",
+        color: "white",
+        borderRadius: "50%",
+        fontSize: "10px",
+        padding: "2px 5px",
+        lineHeight: "1",
+      }}
+    >
+      {basket.totalItems}
+    </span>
+  )}
+
+  <div style={{ fontSize: "0.75rem" }}>{t("cart", { ns: "mobileNavBar" })}</div>
+</div>
+
         {user.isAuth ? (
           <div
             className="text-center"
@@ -65,4 +86,4 @@ const MobileNavBar = () => {
   );
 };
 
-export default MobileNavBar;
+export default observer(MobileNavBar);
