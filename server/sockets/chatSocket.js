7d87d487876fc5358025b089f383d7ab20a1194d
arturz777 <margo310507@gmail.com>
2025-06-07 02:28:41 +0300

    Create chatSocket.js

---
+++ server/sockets/chatSocket.js
@@ -0,0 +1,44 @@
+const { ChatMessage } = require("../models/models");
+
+module.exports = function (io) {
+  io.on("connection", (socket) => {
+    console.log("💬 Новый сокет подключен:", socket.id);
+
+    socket.on("joinChat", (chatId) => {
+      socket.join(`chat_${chatId}`);
+      console.log(`🔗 Socket ${socket.id} присоединился к чату ${chatId}`);
+    });
+
+    socket.on("joinAdminNotifications", () => {
+      socket.join("admin_notifications");
+      console.log(`🛎️ Socket ${socket.id} присоединился к admin_notifications`);
+    });
+
+    socket.on("sendMessage", async (data) => {
+      const { chatId, senderId, senderRole, text } = data;
+
+      try {
+        const newMessage = await ChatMessage.create({
+          chatId,
+          senderId,
+          senderRole,
+          text,
+          isRead: false,
+        });
+
+        io.to(`chat_${chatId}`).emit("receiveMessage", newMessage);
+        io.to("admin_notifications").emit("newChatMessage", newMessage);
+      } catch (err) {
+        console.error("❌ Ошибка при создании сообщения:", err);
+      }
+    });
+
+    socket.on("readMessages", ({ chatId, userId }) => {
+      io.emit("readMessages", { chatId, userId });
+    });
+
+    socket.on("disconnect", () => {
+      console.log("❌ Socket отключился:", socket.id);
+    });
+  });
+};
