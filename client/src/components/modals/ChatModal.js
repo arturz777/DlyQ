24b7920dc54ee04e7ede560a9193fead2bd81da7
arturz777 <margo310507@gmail.com>
2025-06-06 22:53:30 +0300

    Create ChatModal.js

---
+++ client/src/components/modals/ChatModal.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import ChatBox from "../ChatBox";
+import { ChatContext } from "../../context/ChatContext";
+import { Context } from "../../index";
+import styles from "./ChatModal.module.css";
+
+const ChatModal = () => {
+  const { supportChatVisible, supportChatId, closeSupportChat } = useContext(ChatContext);
+  const { user } = useContext(Context);
+
+  if (!supportChatVisible || !user.user) return null;
+
+  return (
+    <div className={styles.modalWrapper}>
+      <div className={styles.chatContainer}>
+        <ChatBox
+          userId={user.user.id}
+          userRole={user.user.role}
+          chatId={supportChatId}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ChatModal;
