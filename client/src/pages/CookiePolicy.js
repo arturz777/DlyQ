eb9aa20f519614af8768f3ed11b7e5af5d8e984c
arturz777 <margo310507@gmail.com>
2025-06-17 05:38:02 +0300

    Update CookiePolicy.js

--- client/src/pages/CookiePolicy.js
+++ client/src/pages/CookiePolicy.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./CookiePolicy.module.css";
 import { useTranslation } from "react-i18next";
+import { useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
 
 const CookiePolicy = () => {
-  const { t } = useTranslation("cookiePolicy"); 
+  const { t } = useTranslation("cookiePolicy");
+  const { openSupportChat } = useContext(ChatContext);
 
   return (
     <div className={styles.container}>
@@ -11,15 +14,21 @@ const CookiePolicy = () => {
       <p>{t("last_updated")}</p>
 
       <section>
-        <h2>{t("sections.what_are_cookies.title")}</h2>
-        <p>{t("sections.what_are_cookies.text")}</p>
+        <h2>{t("what_are_cookies.title")}</h2>
+        <ul>
+          {(t("what_are_cookies.text", { returnObjects: true }) || []).map(
+            (item, i) => (
+              <li key={i}>{item}.</li>
+            )
+          )}
+        </ul>
       </section>
 
       <section>
-        <h2>{t("sections.types_of_cookies.title")}</h2>
-        <p>{t("sections.types_of_cookies.text")}</p>
+        <h2>{t("types_of_cookies.title")}</h2>
+        <p>{t("types_of_cookies.text")}</p>
         <ul>
-          {t("sections.types_of_cookies.list", { returnObjects: true }).map(
+          {t("types_of_cookies.list", { returnObjects: true }).map(
             (item, index) => (
               <li key={index}>{item}</li>
             )
@@ -28,18 +37,35 @@ const CookiePolicy = () => {
       </section>
 
       <section>
-        <h2>{t("sections.disable_cookies.title")}</h2>
-        <p>{t("sections.disable_cookies.text")}</p>
+        <h2>{t("disable_cookies.title")}</h2>
+        <ul>
+          {t("disable_cookies.text", { returnObjects: true }).map(
+            (item, index) => (
+              <li key={index}>{item}</li>
+            )
+          )}
+        </ul>
       </section>
 
       <section>
-        <h2>{t("sections.data_access.title")}</h2>
-        <p>{t("sections.data_access.text")}</p>
+        <h2>{t("data_access.title")}</h2>
+        <ul>
+          {t("data_access.text", { returnObjects: true }).map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
       </section>
 
       <section>
-        <h2>{t("sections.contacts.title")}</h2>
-        <p>{t("sections.contacts.text")}</p>
+        <h2>{t("contact.title")}</h2>
+        <ul>
+          <li>
+            {t("contact.text")}{" "}
+            <span className={styles.chatLink} onClick={openSupportChat}>
+              {t("contact.open_chat")}
+            </span>
+          </li>
+        </ul>
       </section>
     </div>
   );
