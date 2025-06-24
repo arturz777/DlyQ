39a9446e20638b6acef12d7de26c484913673444
arturz777 <margo310507@gmail.com>
2025-06-24 04:57:55 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React, { createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import UserStore from "./store/UserStore";
 import DeviceStore from "./store/DeviceStore";
 import BasketStore from "./store/BasketStore";
@@ -12,13 +13,15 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   
-  root.render(
-    <Context.Provider value={{
-      user: new UserStore(),
-      device: new DeviceStore(),
-      basket: new BasketStore()
-    }}>
-      <App />
-    </Context.Provider>
+   root.render(
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <Context.Provider value={{
+        user: new UserStore(),
+        device: new DeviceStore(),
+        basket: new BasketStore()
+      }}>
+        <App />
+      </Context.Provider>
+    </GoogleOAuthProvider>
   );
 }
