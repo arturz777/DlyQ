d2d92b1b470a300e5bf6c65bd4cd5f4e5611083b
arturz777 <margo310507@gmail.com>
2025-06-28 00:40:38 +0300

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,13 +9,11 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        {/* Логотип и копирайт */}
         <div className={styles.logoSection}>
-          <h2>Zang</h2>
-          <p>© 2024 Zang. {t("all rights reserved", { ns: "footer" })}</p>
+          <h2>DlyQ</h2>
+          <p>© 2024 DlyQ. {t("all rights reserved", { ns: "footer" })}</p>
         </div>
 
-        {/* Полезные ссылки */}
         <div className={styles.linksSection}>
           <Link to="/terms-of-service">
             {t("user Agreement", { ns: "footer" })}
