5f0808baed4772f28f67292acc2abbe494e6aa2b
arturz777 <margo310507@gmail.com>
2025-06-29 23:22:21 +0300

    Update models.js

--- server/models/models.js
+++ server/models/models.js
@@ -41,6 +41,7 @@ const Type = sequelize.define("type", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING, unique: true, allowNull: false },
   img: { type: DataTypes.STRING, allowNull: true },
+  displayOrder: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
 });
 
 const SubType = sequelize.define("subtype", {
