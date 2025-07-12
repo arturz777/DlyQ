c3b554e845b6bb2f169bcdf58885a0afac8dd630
arturz777 <margo310507@gmail.com>
2025-07-12 20:51:19 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,6 +37,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "static")));
+app.use("/static", express.static(path.join(__dirname, "public", "static")));
 app.use(fileUpload({}));
 app.use(cookieParser());
 app.use("/api", router);
