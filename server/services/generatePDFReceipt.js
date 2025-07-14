c4d98a64f0fc2a11f1693c85558a3c270407b647
arturz777 <margo310507@gmail.com>
2025-07-14 15:29:49 +0300

    Update generatePDFReceipt.js

--- server/services/generatePDFReceipt.js
+++ server/services/generatePDFReceipt.js
@@ -1,15 +1,22 @@
 const fs = require("fs");
-const os = require("os");
 const path = require("path");
 const puppeteer = require("puppeteer");
 
 const generatePDFReceipt = async (htmlContent, outputPath) => {
+  const chromeVersionDir = fs.readdirSync('/opt/render/.cache/puppeteer/chrome')[0];
+  const executablePath = path.join(
+    '/opt/render/.cache/puppeteer/chrome',
+    chromeVersionDir,
+    'chrome-linux64',
+    'chrome'
+  );
+
+  console.log("🧭 Используется Chrome по пути:", executablePath);
+
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: path.resolve(
-      '/opt/render/.cache/puppeteer/chrome/linux-138.0.7204.94/chrome-linux64/chrome'
-    )
+    executablePath
   });
 
   const page = await browser.newPage();
@@ -27,3 +34,4 @@ const generatePDFReceipt = async (htmlContent, outputPath) => {
 };
 
 module.exports = generatePDFReceipt;
+
