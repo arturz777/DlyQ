dace128922a0842bf58afb8dcb90630a4b13711c
arturz777 <margo310507@gmail.com>
2025-07-16 22:45:14 +0300

    Update generatePDFReceipt.js

--- server/services/generatePDFReceipt.js
+++ server/services/generatePDFReceipt.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
+const os = require("os");
 const path = require("path");
 const puppeteer = require("puppeteer");
 
 const generatePDFReceipt = async (htmlContent, outputPath) => {
-  console.log("🧭 Запуск Puppeteer со встроенным Chromium");
-
+  console.log("🛠️ Puppeteer executablePath по умолчанию:", puppeteer.executablePath());
   const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
+  headless: "new",
+  executablePath: "/opt/render/.cache/puppeteer/chrome/linux-138.0.7204.94/chrome-linux64/chrome",
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+});
 
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
