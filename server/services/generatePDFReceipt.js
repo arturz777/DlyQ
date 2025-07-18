08aa9da533f58bde15532917bcebc77a6a0a97f9
arturz777 <margo310507@gmail.com>
2025-07-18 10:40:15 +0300

    Update generatePDFReceipt.js

--- server/services/generatePDFReceipt.js
+++ server/services/generatePDFReceipt.js
@@ -5,6 +5,7 @@ const generatePDFReceipt = async (htmlContent, outputPath) => {
 
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath: "/usr/bin/chromium-browser",
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
