c598f26bc67d3cf05be70445a3186b32fef879c0
arturz777 <margo310507@gmail.com>
2025-07-19 12:20:08 +0300

    Update generatePDFShiftBuffer.js

--- server/services/generatePDFShiftBuffer.js
+++ server/services/generatePDFShiftBuffer.js
@@ -1,21 +1,30 @@
 const axios = require("axios");
 
 const generatePDFShiftBuffer = async (html) => {
-  const response = await axios.post(
-    "https://api.pdfshift.io/v3/convert/html",
-    { source: html },
-    {
-      auth: {
-        username: process.env.PDFSHIFT_API_KEY, 
-      },
-      responseType: "arraybuffer",
-    }
-  );
+  try {
+    const response = await axios.post(
+      "https://api.pdfshift.io/v3/convert/html",
+      html,
+      {
+        auth: {
+          username: process.env.PDFSHIFT_API_KEY,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+        responseType: "arraybuffer",
+      }
+    );
 
-  return response.data; 
+    return response.data;
+  } catch (error) {
+    console.error("❌ PDFShift response:", error.response?.data || error.message);
+    throw error;
+  }
 };
 
 module.exports = generatePDFShiftBuffer;
+
 
 
 
