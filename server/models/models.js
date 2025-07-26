ef0af80e510a01b714f2ab89731a47d90b14cc41
arturz777 <margo310507@gmail.com>
2025-07-26 17:27:33 +0300

    Update models.js

--- server/models/models.js
+++ server/models/models.js
@@ -23,8 +23,8 @@ const BasketDevice = sequelize.define("basket_device", {
 const Device = sequelize.define("device", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING, unique: true, allowNull: false },
-  price: { type: DataTypes.INTEGER, allowNull: false },
-  oldPrice: { type: DataTypes.INTEGER, allowNull: true },
+  price: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
+oldPrice: { type: DataTypes.DECIMAL(10, 2), allowNull: true },
   rating: { type: DataTypes.INTEGER, defaultValue: 0 },
   img: { type: DataTypes.STRING, allowNull: false },
   thumbnails: { type: DataTypes.JSONB, allowNull: true, defaultValue: [] },
