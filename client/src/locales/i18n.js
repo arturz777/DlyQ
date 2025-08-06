e61a0cb111fc0b4839c6593fbfc725e48382d1f8
arturz777 <margo310507@gmail.com>
2025-08-06 19:18:54 +0300

    Update i18n.js

--- client/src/locales/i18n.js
+++ client/src/locales/i18n.js
@@ -152,7 +152,6 @@ i18n
     },
 
     fallbackLng: "est", // Язык по умолчанию
-    lng: "est", // 👉 язык по умолчанию
     defaultNS: "navbar",
     debug: false, // Включить отладку
     interpolation: {
