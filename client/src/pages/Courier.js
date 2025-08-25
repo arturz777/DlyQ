fe87c07d8ccca14c3a92315784e35190da46a825
arturz777 <margo310507@gmail.com>
2025-08-25 23:04:07 +0300

    Update Courier.js

--- client/src/pages/Courier.js
+++ client/src/pages/Courier.js
@@ -24,7 +24,7 @@ import {
 } from "../http/courierAPI";
 import styles from "./Courier.module.css";
 
-const socket = io("https://zang-4.onrender.com");
+const socket = io("https://dlyq-backend-staging.onrender.com");
 
 const WAREHOUSE_LOCATION = { lat: 59.51372, lng: 24.828888 };
 
