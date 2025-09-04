417d0f909da49a1216556f8ca3deb477e5c583f6
arturz777 <margo310507@gmail.com>
2025-09-04 23:23:49 +0300

    Update deviceController.js

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -56,7 +56,7 @@ class DeviceController {
         throw new Error("Ошибка загрузки изображения в Supabase Storage");
       }
 
-      const publicURL = `https://ujsitjkochexlcqrwxan.supabase.co/storage/v1/object/public/images/${fileName}`;
+      const publicURL = `https://esjsdctbiuzornxbktjb.supabase.co/storage/v1/object/public/images/${fileName}`;
 
       let thumbnails = [];
       if (req.files && req.files.thumbnails) {
@@ -79,7 +79,7 @@ class DeviceController {
               return null;
             }
 
-            return `https://ujsitjkochexlcqrwxan.supabase.co/storage/v1/object/public/images/${thumbFileName}`;
+            return `https://esjsdctbiuzornxbktjb.supabase.co/storage/v1/object/public/images/${thumbFileName}`;
           })
         );
 
@@ -651,7 +651,7 @@ class DeviceController {
           });
         }
 
-        fileName = `https://ujsitjkochexlcqrwxan.supabase.co/storage/v1/object/public/images/${newFileName}`;
+        fileName = `https://esjsdctbiuzornxbktjb.supabase.co/storage/v1/object/public/images/${newFileName}`;
       }
 
       if (existingImages.length === 0) {
@@ -689,7 +689,7 @@ class DeviceController {
               return null;
             }
 
-            return `https://ujsitjkochexlcqrwxan.supabase.co/storage/v1/object/public/images/${thumbFileName}`;
+            return `https://esjsdctbiuzornxbktjb.supabase.co/storage/v1/object/public/images/${thumbFileName}`;
           })
         );
 
