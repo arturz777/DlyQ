ae113ef06973bba038888ca8db8f124690c932a6
arturz777 <margo310507@gmail.com>
2025-09-05 19:57:12 +0300

    Update deviceRouter.js

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,18 +1,31 @@
-// server/routes/deviceRouter.js
-const Router = require('express')
-const router = new Router()
-const deviceController = require('../controllers/deviceController')
+const Router = require('express');
+const router = new Router();
+const deviceController = require('../controllers/deviceController');
+const makeController = require('../controllers/makeController');
+const modelController = require('../controllers/modelController');
+
 
 router.put("/updateNewStatus", deviceController.updateNewStatus);
 router.get("/discounted", deviceController.getDiscountedDevices);
 router.put("/updateDiscountStatus", deviceController.update);
+router.post("/check-stock", deviceController.checkStock);
+router.get('/search', deviceController.search);
+
+router.get('/make', makeController.getAll);
+router.post('/make', makeController.create);
+router.put('/make/:id', makeController.update);
+router.delete('/make/:id', makeController.delete);
+
+router.get('/model', modelController.getByMake);
+router.post('/model', modelController.create);
+router.put('/model/:id', modelController.update);
+router.delete('/model/:id', modelController.delete);
+
 router.post('/', deviceController.create)
 router.get('/', deviceController.getAll)
-router.get('/search', deviceController.search);
-router.get('/:id', deviceController.getOne)
-router.delete('/:id', deviceController.delete);
-router.put('/:id', deviceController.update);
-router.post("/check-stock", deviceController.checkStock);
 router.post('/:id/stock', deviceController.adjustStock);
+router.get('/:id', deviceController.getOne)
+router.put('/:id', deviceController.update);
+router.delete('/:id', deviceController.delete);
 
 module.exports = router
