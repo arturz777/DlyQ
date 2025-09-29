9e62c27ee927bb9b9336badf7fa161286f95530f
arturz777 <margo310507@gmail.com>
2025-09-29 19:44:26 +0300

    Create api.js

---
+++ MobileApp/src/config/api.js
@@ -0,0 +1,4 @@
+export const BASE_URL   = __DEV__ ? "http://localhost:5000" : "https://your-prod.example.com";
+export const API_URL    = `${BASE_URL}/api`;
+export const SOCKET_URL = BASE_URL;      
+export const SOCKET_PATH = "/socket.io";
