d50358d4679ad8bef999029aa4473186da31822a
arturz777 <margo310507@gmail.com>
2025-10-10 06:30:14 +0300

    Update emailService.js

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,1 +1,31 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'margo310507@gmail.com', 
+    pass: 'xbiw laxs btvo khhr', 
+  },
+  tls: {
+    rejectUnauthorized: false, 
+  },
+});
+
+const sendEmail = async (to, subject, html, attachments = []) => {
+  try {
+    await transporter.sendMail({
+      from: '"DlyQ" <margo310507@gmail.com>',
+      to,
+      subject,
+      html,
+      attachments,
+    });
+    console.log("✅ Письмо отправлено на:", to);
+  } catch (error) {
+    console.error("❌ Ошибка отправки письма:", error);
+  }
+};
+
+
+module.exports = sendEmail;
 
