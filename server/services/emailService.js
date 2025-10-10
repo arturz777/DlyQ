5a8b3baee118da9b42456ee0b5938bf304a7edc3
arturz777 <margo310507@gmail.com>
2025-10-10 20:15:12 +0300

    Update emailService.js

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,27 +1,26 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.zoho.eu',
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
-  tls: { rejectUnauthorized: false } 
+  service: 'gmail',
+  auth: {
+    user: 'margo310507@gmail.com', 
+    pass: 'xbiw laxs btvo khhr', 
+  },
+  tls: {
+    rejectUnauthorized: false, 
+  },
 });
 
 const sendEmail = async (to, subject, html, attachments = []) => {
- 
   try {
-     if (!process.env.MAIL_USER || !process.env.MAIL_PASS) {
-    throw new Error('MAIL_USER / MAIL_PASS не заданы в окружении');
-  }
     await transporter.sendMail({
-      from: `"DLYQ OÜ" <${process.env.MAIL_USER}>`,
+      from: '"DlyQ" <margo310507@gmail.com>',
       to,
       subject,
       html,
       attachments,
     });
+    console.log("✅ Письмо отправлено на:", to);
   } catch (error) {
     console.error("❌ Ошибка отправки письма:", error);
   }
