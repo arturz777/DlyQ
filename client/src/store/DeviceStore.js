aca19991f535f3a80639ce213a67fadcb7d60583
arturz777 <margo310507@gmail.com>
2025-10-14 05:34:25 +0300

    Update DeviceStore.js

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -15,7 +15,7 @@ export default class DeviceStore {
     this._selectedBrand = {};
     this._page = 1;
     this._totalCount = 0;
-    this._limit = 1000;
+    this._limit = 50;
     this._facets = { subtypes: [], brands: [] };
     this._loading = { devices: false, subtypes: false };
     makeAutoObservable(this);
