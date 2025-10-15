b7d3985a5e2ec820bacb1750ff73d7cfe67a1fe5
arturz777 <margo310507@gmail.com>
2025-10-15 03:24:59 +0300

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -182,6 +182,7 @@ const HomePage = () => {
         </div>
       )}
       <div className={styles.banner}>
+            {/* <h1>{t("fast delivery", { ns: "homePage" })}</h1>  */}
         <p>{t("average delivery time: 15–30 minutes", { ns: "homePage" })}</p>
       </div>
 
