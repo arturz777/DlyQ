6a3ec8f00c0d79e286a7ec23683e579f14cbf4a8
arturz777 <margo310507@gmail.com>
2025-10-17 01:32:05 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,10 @@ import NavBar from "./components/NavBar";
 import { observer } from "mobx-react-lite";
 import { Context } from "./index";
 import { check } from "./http/userAPI";
+import { fetchMaintenance } from "./http/configAPI";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Elements } from "@stripe/react-stripe-js"; 
+import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import MobileNavBar from "./components/MobileNavBar";
 import OrderSidebar from "./components/OrderSidebar";
@@ -17,8 +18,8 @@ import CookieConsent from "./components/modals/CookieConsent";
 import LoadingBar from "./components/LoadingBar";
 import ChatModal from "./components/modals/ChatModal";
 import { ChatContext } from "./context/ChatContext";
-import './locales/i18n';
-import "./App.css"; 
+import "./locales/i18n";
+import "./App.css";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
@@ -26,77 +27,93 @@ const App = observer(() => {
   const { user } = useContext(Context);
   const [loading, setLoading] = useState(true);
   const [supportChatVisible, setSupportChatVisible] = useState(false);
-  const [supportChatId, setSupportChatId] = useState(1); 
+  const [supportChatId, setSupportChatId] = useState(1);
+
+  const isAdmin =
+    user.isAuth && (user.user?.role === "ADMIN" || user.user?.role === "admin");
+  const maintenanceKnown = appStore.isMaintenanceKnown;
+  const maintenanceActive =
+    maintenanceKnown && appStore.maintenance.enabled && !isAdmin;
 
   const fetchSupportChat = async (userId) => {
-  const res = await fetch(`https://dlyq-backend-staging.onrender.com/api/chat/support-chat`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ userId }),
-  });
-  return await res.json();
-};
+    const res = await fetch("https://dlyq-backend-staging.onrender.com/api/chat/support-chat", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userId }),
+    });
+    return await res.json();
+  };
 
-const openSupportChat = () => {
-  setSupportChatId(null); 
-  setSupportChatVisible(true);
-};
+  const openSupportChat = () => {
+    setSupportChatId(null);
+    setSupportChatVisible(true);
+  };
 
   const closeSupportChat = () => setSupportChatVisible(false);
 
   useEffect(() => {
-    const token = localStorage.getItem("token"); 
+    const token = localStorage.getItem("token");
     appStore.setIsLoading(true);
+
+    fetchMaintenance()
+      .then(appStore.setMaintenance.bind(appStore))
+      .catch(() => {});
 
     if (!token) {
       setLoading(false);
       appStore.setIsLoading(false);
-      user.setUser({}); 
-      user.setIsAuth(false); 
-      return; 
+      user.setUser({});
+      user.setIsAuth(false);
+      return;
     }
 
     check()
-  .then((data) => {
-    if (data) {
-      user.setUser(data);
-      user.setIsAuth(true);
-    } else {
-      user.setUser({});
-      user.setIsAuth(false);
-    }
-  })
-  .finally(() => {
-    setLoading(false);
-    appStore.setIsLoading(false);
-  });
+      .then((data) => {
+        if (data) {
+          user.setUser(data);
+          user.setIsAuth(true);
+        } else {
+          user.setUser({});
+          user.setIsAuth(false);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+        appStore.setIsLoading(false);
+      });
   }, [user]);
 
   if (loading) {
     return <LoadingBar />;
   }
-  
+
   return (
-    <ChatContext.Provider value={{
-      supportChatVisible,
-      supportChatId,
-      openSupportChat,
-      closeSupportChat
-    }}>
-    <BrowserRouter>
-     <Elements stripe={stripePromise}>
-     <LoadingBar />
-      <NavBar />
-      <AppRouter />
-      <ToastContainer position="top-right" autoClose={3000} />
-      <OrderSidebar />
-      </Elements>
-      <MobileNavBar />
-      <CookieConsent />
-      <ChatModal />
-      <Footer />
-     </BrowserRouter>
-     </ChatContext.Provider>
+    <ChatContext.Provider
+      value={{
+        supportChatVisible,
+        supportChatId,
+        openSupportChat,
+        closeSupportChat,
+      }}
+    >
+      <BrowserRouter>
+        <Elements stripe={stripePromise}>
+          {!maintenanceActive && <LoadingBar />}
+          {!maintenanceActive && <NavBar />}
+          <AppRouter />
+          {!maintenanceActive && maintenanceKnown && (
+            <>
+              <ToastContainer position="top-right" autoClose={3000} />
+              <OrderSidebar />
+            </>
+          )}
+        </Elements>
+        {!maintenanceActive && maintenanceKnown && <MobileNavBar />}
+        {!maintenanceActive && maintenanceKnown && <CookieConsent />}
+        {!maintenanceActive && maintenanceKnown && <ChatModal />}
+        {!maintenanceActive && maintenanceKnown && <Footer />}
+      </BrowserRouter>
+    </ChatContext.Provider>
   );
 });
 
