88ce60a8423d5e99c0d2c0b2740456cf7f97f235
arturz777 <margo310507@gmail.com>
2025-10-21 13:31:00 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,14 +16,40 @@ const warehouseRouter = require("./routes/warehouseRouter");
 const orderRouter = require("./routes/orderRouter");
 const chatRouter = require("./routes/chatRouter");
 const cookieParser = require("cookie-parser");
+const Stripe = require("stripe");  
+const paymentsRouter = require("./routes/paymentsRouter.js");
 
 setupCleanupTask();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.post(
+  "/api/stripe/webhook",
+  express.raw({ type: "application/json" }),
+  (req, res) => {
+    try {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+      const sig = req.headers["stripe-signature"];
+      const event = stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+
+      if (event.type === "payment_intent.succeeded") {
+        const pi = event.data.object;
+      }
+
+      res.json({ received: true });
+    } catch (err) {
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+  }
+);
+
 const allowedOrigins = [
-  "https://dlyq.ee",
+  "https://dlyq.ee",    //Proda
   "https://www.dlyq.ee",
 ];
 
@@ -77,6 +103,7 @@ app.use("/api/couriers", courierRouter);
 app.use("/api/warehouse", warehouseRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/chat", chatRouter);
+app.use("/api/payments", paymentsRouter);
 
 const chatSocket = require("./sockets/chatSocket");
 chatSocket(io);
