7257c68421e7d9c1952a1f30a5913faecf31a2d0
arturz777 <margo310507@gmail.com>
2025-11-02 14:54:37 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -27,22 +27,4 @@ if (rootElement) {
       </Context.Provider>
     </GoogleOAuthProvider>
   );
-
-    if ("serviceWorker" in navigator) {
-    if (process.env.NODE_ENV === "production") {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/sw.js")
-          .then((r) => console.log("SW registered:", r.scope))
-          .catch(console.error);
-      });
-    } else {
-      navigator.serviceWorker.getRegistrations?.().then((regs) => {
-        regs.forEach((r) => r.unregister());
-      });
-      if (window.caches?.keys) {
-        caches.keys().then((keys) => keys.forEach((k) => caches.delete(k)));
-      }
-    }
-  }
 }
