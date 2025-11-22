6faf5832f47944f577a58eee77467de1c81318aa
arturz777 <margo310507@gmail.com>
2025-11-22 20:15:48 +0200

    Update courierRouter.js

--- server/routes/courierRouter.js
+++ server/routes/courierRouter.js
@@ -34,6 +34,6 @@ router.get(
 );
 router.get("/me", authMiddleware, courierController.getSelf);
 router.post("/push-token", authMiddleware, courierController.savePushToken);
-
+router.post('/orders/:id/decline', authMiddleware, courierController.declineOrder);
 
 module.exports = router;
