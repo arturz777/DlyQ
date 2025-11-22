6ccafb87a1b6cecb061c9c2532fce8730ed1f427
arturz777 <margo310507@gmail.com>
2025-11-22 18:34:22 +0200

    Update courierRouter.js

--- server/routes/courierRouter.js
+++ server/routes/courierRouter.js
@@ -2,17 +2,38 @@ const Router = require("express");
 const router = new Router();
 const courierController = require("../controllers/courierController");
 const authMiddleware = require("../middleware/authMiddleware");
-const checkRole = require('../middleware/checkRoleMiddleware');
-
+const checkRole = require("../middleware/checkRoleMiddleware");
 
 router.get("/orders", authMiddleware, courierController.getActiveOrders);
-router.post("/orders/:id/accept", authMiddleware, courierController.acceptOrder);
+router.post(
+  "/orders/:id/accept",
+  authMiddleware,
+  courierController.acceptOrder
+);
 router.post("/status", authMiddleware, courierController.toggleCourierStatus);
-router.post("/orders/:id/complete", authMiddleware, courierController.completeDelivery);
-router.post("/orders/:id/status", authMiddleware, courierController.updateDeliveryStatus);
-router.post("/update-location", authMiddleware, courierController.updateCourierLocation);
-router.get('/couriers', authMiddleware, checkRole("ADMIN"), courierController.getAllCouriers);
+router.post(
+  "/orders/:id/complete",
+  authMiddleware,
+  courierController.completeDelivery
+);
+router.post(
+  "/orders/:id/status",
+  authMiddleware,
+  courierController.updateDeliveryStatus
+);
+router.post(
+  "/update-location",
+  authMiddleware,
+  courierController.updateCourierLocation
+);
+router.get(
+  "/couriers",
+  authMiddleware,
+  checkRole("ADMIN"),
+  courierController.getAllCouriers
+);
 router.get("/me", authMiddleware, courierController.getSelf);
+router.post("/push-token", authMiddleware, courierController.savePushToken);
 
 
 module.exports = router;
