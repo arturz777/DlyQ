07f2e716d1ef53e38842eb124e7313e44ee2b18b
arturz777 <margo310507@gmail.com>
2025-11-25 13:34:49 +0200

    Update courierRouter.js

--- server/routes/courierRouter.js
+++ server/routes/courierRouter.js
@@ -1,78 +1,28 @@
 const Router = require("express");
 const router = new Router();
-const courierController = require("../controllers/courierController");
+const warehouseController = require("../controllers/warehouseController");
 const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
-const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
 
 router.get(
   "/orders",
   authMiddleware,
-  roleMiddleware("COURIER"),
-  courierController.getActiveOrders
+  roleMiddleware("WAREHOUSE"),
+  warehouseController.getWarehouseOrders
 );
 
 router.post(
   "/orders/:id/accept",
   authMiddleware,
-  roleMiddleware("COURIER"),
-  courierController.acceptOrder
-);
-
-router.post(
-  "/status",
-  authMiddleware,
-  roleMiddleware("COURIER"),
-  courierController.toggleCourierStatus
+  roleMiddleware("WAREHOUSE"),
+  warehouseController.acceptOrder
 );
 
 router.post(
   "/orders/:id/complete",
   authMiddleware,
-  roleMiddleware("COURIER"),
-  courierController.completeDelivery
-);
-
-router.post(
-  "/orders/:id/status",
-  authMiddleware,
-  roleMiddleware("COURIER"),
-  courierController.updateDeliveryStatus
-);
-
-router.post(
-  "/update-location",
-  authMiddleware,
-  roleMiddleware("COURIER"),
-  courierController.updateCourierLocation
-);
-
-router.get(
-  "/me",
-  authMiddleware,
-  roleMiddleware("COURIER"),
-  courierController.getSelf
-);
-
-router.post(
-  "/push-token",
-  authMiddleware,
-  roleMiddleware("COURIER"),
-  courierController.savePushToken
-);
-
-router.post(
-  "/orders/:id/decline",
-  authMiddleware,
-  roleMiddleware("COURIER"),
-  courierController.declineOrder
-);
-
-router.get(
-  "/couriers",
-  authMiddleware,
-  checkRoleMiddleware("ADMIN"),
-  courierController.getAllCouriers
+  roleMiddleware("WAREHOUSE"),
+  warehouseController.completeOrder
 );
 
 module.exports = router;
