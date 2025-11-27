51d13a8dcb40d7cb0991f3927f441fb5824684d9
arturz777 <margo310507@gmail.com>
2025-11-27 21:01:13 +0200

    Update warehouseController.js

--- server/controllers/warehouseController.js
+++ server/controllers/warehouseController.js
@@ -1,9 +1,6 @@
 const { Order, Warehouse } = require("../models/models");
 const { Op } = require("sequelize");
-const {
-  sendOrderToNextCourier,
-} = require("../services/orderDistributionService");
-const { sendOrderAssignedPush } = require("../services/pushService");
+const { sendOrderToNextCourier } = require("../services/orderDistributionService");
 
 function buildWarehouseName(user) {
   const parts = [];
@@ -44,7 +41,7 @@ class WarehouseController {
         order: [["createdAt", "DESC"]],
       });
 
-      const formattedOrders = orders.map((order) => ({
+      const formattedOrders = orders.map(order => ({
         ...order.toJSON(),
         orderDetails: order.orderDetails ? JSON.parse(order.orderDetails) : [],
         preorderDate: order.desiredDeliveryDate || null,
@@ -94,7 +91,7 @@ class WarehouseController {
       io.emit("orderStatusUpdate", order);
 
       try {
-        await sendOrderAssignedPush(order);
+        await sendOrderToNextCourier(order);
       } catch (err) {
         console.error("push error (warehouse.acceptOrder):", err);
       }
