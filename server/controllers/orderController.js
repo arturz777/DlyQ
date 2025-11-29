f2021cdf631463973a63b75c0f7d2c5792231196
arturz777 <margo310507@gmail.com>
2025-11-29 12:48:04 +0200

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -8,7 +8,10 @@ const { t } = require("../utils/translations");
 const getDistanceFromWarehouse = require("../utils/distance");
 const generatePDFShiftBuffer = require("../services/generatePDFShiftBuffer");  // Proda (PDFShift)
 const { supabase } = require("../config/supabaseClient");
-const { sendOrderAssignedPush } = require("../services/pushService");
+const {
+  sendOrderAssignedPush,
+  sendNewOrderPushToWarehouse,
+} = require("../services/pushService");
 const { sendOrderToNextCourier } = require("../services/orderDistributionService");
 const uuid = require("uuid");
 
