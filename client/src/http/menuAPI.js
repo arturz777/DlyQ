6efabbb44dca3918867231b88fc95b6c47b4b9df
arturz777 <margo310507@gmail.com>
2025-12-13 14:35:11 +0200

    Update menuAPI.js

--- client/src/http/menuAPI.js
+++ client/src/http/menuAPI.js
@@ -1,49 +1,49 @@
 import { $authHost, $host } from "./index";
 
 export const fetchMenuCategories = async (sellerId) => {
-  const { data } = await $host.get("/api/menu/categories", {
+  const { data } = await $host.get("/menu/categories", {
     params: { sellerId },
   });
   return data;
 };
 
 export const createMenuCategory = async (body) => {
-  const { data } = await $authHost.post("/api/menu/categories", body);
+  const { data } = await $authHost.post("/menu/categories", body);
   return data;
 };
 
 export const updateMenuCategory = async (id, body) => {
-  const { data } = await $authHost.put(`/api/menu/categories/${id}`, body);
+  const { data } = await $authHost.put(`/menu/categories/${id}`, body);
   return data;
 };
 
 export const deactivateMenuCategory = async (id, sellerId) => {
   const { data } = await $authHost.patch(
-    `/api/menu/categories/${id}/deactivate`,
+    `/menu/categories/${id}/deactivate`,
     { sellerId }
   );
   return data;
 };
 
 export const fetchMenuItems = async (sellerId) => {
-  const { data } = await $host.get("/api/menu/items", {
+  const { data } = await $host.get("/menu/items", {
     params: { sellerId },
   });
   return data;
 };
 
 export const createMenuItem = async (body) => {
-  const { data } = await $authHost.post("/api/menu/items", body);
+  const { data } = await $authHost.post("/menu/items", body);
   return data;
 };
 
 export const updateMenuItem = async (id, body) => {
-  const { data } = await $authHost.put(`/api/menu/items/${id}`, body);
+  const { data } = await $authHost.put(`/menu/items/${id}`, body);
   return data;
 };
 
 export const toggleMenuItemAvailability = async (id, sellerId, isAvailable) => {
-  const { data } = await $authHost.patch(`/api/menu/items/${id}/availability`, {
+  const { data } = await $authHost.patch(`/menu/items/${id}/availability`, {
     sellerId,
     isAvailable,
   });
@@ -51,7 +51,7 @@ export const toggleMenuItemAvailability = async (id, sellerId, isAvailable) => {
 };
 
 export const deactivateMenuItem = async (id, sellerId) => {
-  const { data } = await $authHost.patch(`/api/menu/items/${id}/deactivate`, {
+  const { data } = await $authHost.patch(`/menu/items/${id}/deactivate`, {
     sellerId,
   });
   return data;
