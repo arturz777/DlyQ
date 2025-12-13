56eeba90759b56047cd442f26cea39f59d4a7c7e
arturz777 <margo310507@gmail.com>
2025-12-13 11:49:35 +0200

    Update warehouseRouter.js

--- server/routes/warehouseRouter.js
+++ server/routes/warehouseRouter.js
@@ -7,28 +7,28 @@ const roleMiddleware = require("../middleware/roleMiddleware");
 router.get(
   "/orders",
   authMiddleware,
-  roleMiddleware("WAREHOUSE"),
+  roleMiddleware("WAREHOUSE", "SELLER", "ADMIN"),
   warehouseController.getWarehouseOrders
 );
 
 router.post(
   "/orders/:id/accept",
   authMiddleware,
-  roleMiddleware("WAREHOUSE"),
+  roleMiddleware("WAREHOUSE", "SELLER", "ADMIN"),
   warehouseController.acceptOrder
 );
 
 router.post(
   "/orders/:id/complete",
   authMiddleware,
-  roleMiddleware("WAREHOUSE"),
+  roleMiddleware("WAREHOUSE", "SELLER", "ADMIN"),
   warehouseController.completeOrder
 );
 
 router.post(
   "/push-token",
   authMiddleware,
-  roleMiddleware("WAREHOUSE"),
+  roleMiddleware("WAREHOUSE", "SELLER", "ADMIN"),
   warehouseController.savePushToken
 );
 
