02d6f50bf69915dd9a55dc9f5390728bc4090da1
arturz777 <margo310507@gmail.com>
2025-12-21 19:28:59 +0200

    Update Shop.js

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -268,6 +268,7 @@ const Shop = () => {
       )}
 
       <div className={styles.banner}>
+        // <h1>{t("fast delivery", { ns: "homePage" })}</h1>
         <p>{t("average delivery time: 15–30 minutes", { ns: "homePage" })}</p>
       </div>
 
