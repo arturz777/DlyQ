d8983d7000bfa6670cd1e8027d650def8a57caef
arturz777 <margo310507@gmail.com>
2025-12-23 11:52:25 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -31,12 +31,15 @@ const AppLayout = observer(() => {
     location.pathname === "/courier" ||
     location.pathname === "/warehouse";
 
+    const hideDesktopNavBar =
+  hideLayout || location.pathname.startsWith("/catalog");
+
   return (
     <>
       {appStore.isLoading && <LoadingBar />}
 
       <Elements stripe={stripePromise}>
-        {!hideLayout && <NavBar />}
+        {!hideDesktopNavBar && <NavBar />}
         <AppRouter />
         {!hideLayout && <OrderSidebar />}
       </Elements>
@@ -77,7 +80,7 @@ const App = observer(() => {
 
   const fetchSupportChat = async (userId) => {
     const res = await fetch(
-      `${process.env.REACT_APP_API_URL}/chat/support-chat`,
+      `${process.env.REACT_APP_API_URL}api/chat/support-chat`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
