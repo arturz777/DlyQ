f8d97460017ba800f776f16eeef67d64600be227
arturz777 <margo310507@gmail.com>
2025-12-25 00:29:36 +0200

    Update foodCatalogAPI.js

--- client/src/http/foodCatalogAPI.js
+++ client/src/http/foodCatalogAPI.js
@@ -1,6 +1,6 @@
 import { $host } from "./index";
 
 export const searchFood = async (q, limit = 30) => {
-  const { data } = await $host.get("api/food-catalog/search", { params: { q, limit } });
+  const { data } = await $host.get("/food-catalog/search", { params: { q, limit } });
   return data;
 };
