9983a4b7b640fb62d816f28dc2f00a533523cc4b
arturz777 <margo310507@gmail.com>
2025-12-31 14:05:24 +0200

    Update orderRouter.js

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -1,19 +1,32 @@
-const Router = require('express');
+const Router = require("express");
 const router = new Router();
-const orderController = require('../controllers/orderController');
-const authMiddleware = require('../middleware/authMiddleware');
-const checkRole = require('../middleware/checkRoleMiddleware');
+const orderController = require("../controllers/orderController");
+const authMiddleware = require("../middleware/authMiddleware");
+const checkRole = require("../middleware/checkRoleMiddleware");
 
-
-router.post('/create', authMiddleware, orderController.createOrder);
-router.get('/user', authMiddleware, orderController.getUserOrders);
-router.put('/update-status', authMiddleware, orderController.updateOrderStatus);
-router.get('/active', authMiddleware, orderController.getActiveOrder);
+router.post("/create", authMiddleware, orderController.createOrder);
+router.get("/user", authMiddleware, orderController.getUserOrders);
+router.put("/update-status", authMiddleware, orderController.updateOrderStatus);
+router.get("/active", authMiddleware, orderController.getActiveOrder);
 router.get("/delivery-cost", orderController.getDeliveryCost);
-router.get('/admin', authMiddleware, checkRole("ADMIN"), orderController.getAllOrdersForAdmin);
-router.put('/:id/status', authMiddleware, checkRole("ADMIN"), orderController.adminUpdateOrderStatus);
-router.put('/:id/assign-courier', authMiddleware, checkRole("ADMIN"), orderController.assignCourier);
-router.get('/:id/receipt', orderController.downloadReceipt);
-
+router.get(
+  "/admin",
+  authMiddleware,
+  checkRole("ADMIN"),
+  orderController.getAllOrdersForAdmin
+);
+router.put(
+  "/:id/status",
+  authMiddleware,
+  checkRole("ADMIN"),
+  orderController.adminUpdateOrderStatus
+);
+router.put(
+  "/:id/assign-courier",
+  authMiddleware,
+  checkRole("ADMIN"),
+  orderController.assignCourier
+);
+router.get("/:id/receipt", orderController.downloadReceipt);
 
 module.exports = router;
