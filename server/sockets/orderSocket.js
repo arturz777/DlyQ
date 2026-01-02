431bded3716649b1cd3907836d752454e2b8a217
arturz777 <margo310507@gmail.com>
2026-01-03 00:53:27 +0200

    Update orderSocket.js

--- server/sockets/orderSocket.js
+++ server/sockets/orderSocket.js
@@ -1,5 +1,7 @@
 module.exports = function orderSocket(io, socket) {
   socket.on("joinOrderRoom", ({ orderId }) => {
+    console.log("➡️ joinOrderRoom request:", socket.id, orderId, "rooms(before):", [...socket.rooms]);
+
     if (!orderId) return;
 
     for (const r of socket.rooms) {
@@ -8,13 +10,16 @@ module.exports = function orderSocket(io, socket) {
 
     const room = `order:${orderId}`;
     socket.join(room);
-    console.log("✅ joinOrderRoom:", socket.id, room);
+
+    console.log("✅ joinOrderRoom:", socket.id, room, "rooms(after):", [...socket.rooms]);
   });
 
   socket.on("leaveOrderRoom", ({ orderId }) => {
+    console.log("⬅️ leaveOrderRoom:", socket.id, orderId, "rooms(before):", [...socket.rooms]);
     if (!orderId) return;
     const room = `order:${orderId}`;
     socket.leave(room);
-    console.log("👋 leaveOrderRoom:", socket.id, room);
+    console.log("👋 left:", socket.id, room, "rooms(after):", [...socket.rooms]);
   });
 };
+
