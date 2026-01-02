a4eb4c2a37366b30855c49c7c7f4a672796558f2
arturz777 <margo310507@gmail.com>
2026-01-02 23:30:30 +0200

    Create socket.js

---
+++ client/src/socket.js
@@ -0,0 +1,7 @@
+import { io } from "socket.io-client";
+
+export const socket = io(process.env.REACT_APP_API_URL, {
+  transports: ["websocket"],
+  withCredentials: true,
+  autoConnect: true,
+});
