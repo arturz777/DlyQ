e351e0c0104467ccd2072854f2848e367cef4204
arturz777 <margo310507@gmail.com>
2026-01-03 01:03:37 +0200

    Update index.js

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -3,18 +3,20 @@ const orderSocket = require("./orderSocket");
 const warehouseSocket = require("./warehouseSocket");
 
 module.exports = function registerSockets(io) {
-  io.on("connection", (socket) => {
-    console.log("🔌 socket connected:", socket.id, "origin:", socket.handshake.headers.origin);
+ io.on("connection", (socket) => {
+  console.log("🔌 connect", socket.id, "transport:", socket.conn.transport.name);
 
-    socket.onAny((event, ...args) => {
-      if (["joinOrderRoom","leaveOrderRoom","joinWarehouseRoom"].includes(event)) {
-        console.log("📩 onAny:", event, "from", socket.id, "args:", args);
-      }
-    });
-
-    socket.on("disconnect", (reason) => {
-      console.log("❌ socket disconnected:", socket.id, "reason:", reason);
-    });
+  socket.conn.on("upgrade", (transport) => {
+    console.log("⬆️ upgraded", socket.id, "to:", transport.name);
   });
+
+  socket.conn.on("close", (reason) => {
+    console.log("🧨 engine close", socket.id, "reason:", reason);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log("❌ disconnect", socket.id, "reason:", reason, "transport:", socket.conn.transport?.name);
+  });
+});
 };
 
