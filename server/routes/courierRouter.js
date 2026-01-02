c04ffa279fdf3a1d6ac491742c97b00fcbceea5c
arturz777 <margo310507@gmail.com>
2026-01-02 21:01:55 +0200

    Update courierRouter.js

--- server/routes/courierRouter.js
+++ server/routes/courierRouter.js
@@ -1,5 +1,4 @@
-const Router = require("express");
-const router = new Router();
+const router = require("express").Router();
 const courierController = require("../controllers/courierController");
 const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
