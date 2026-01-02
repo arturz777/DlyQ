2584e3d65332419a53a559c1a2540a947af649e1
arturz777 <margo310507@gmail.com>
2026-01-03 00:52:28 +0200

    Update index.js

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -4,14 +4,17 @@ const warehouseSocket = require("./warehouseSocket");
 
 module.exports = function registerSockets(io) {
   io.on("connection", (socket) => {
-    console.log("🔌 socket connected:", socket.id);
+    console.log("🔌 socket connected:", socket.id, "origin:", socket.handshake.headers.origin);
 
-    chatSocket(io, socket);
-    orderSocket(io, socket);
-    warehouseSocket(io, socket);
+    socket.onAny((event, ...args) => {
+      if (["joinOrderRoom","leaveOrderRoom","joinWarehouseRoom"].includes(event)) {
+        console.log("📩 onAny:", event, "from", socket.id, "args:", args);
+      }
+    });
 
-    socket.on("disconnect", () => {
-      console.log("❌ socket disconnected:", socket.id);
+    socket.on("disconnect", (reason) => {
+      console.log("❌ socket disconnected:", socket.id, "reason:", reason);
     });
   });
 };
+
