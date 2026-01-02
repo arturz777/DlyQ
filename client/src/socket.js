f6d0332efea31a1f6445c166e2a011f3d60b1468
arturz777 <margo310507@gmail.com>
2026-01-03 01:05:49 +0200

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,11 @@
 import { io } from "socket.io-client";
 
 export const socket = io(process.env.REACT_APP_API_URL, {
-  transports: ["websocket"],
   withCredentials: true,
-  autoConnect: true,
+  transports: ["websocket"],      // 👈 важно
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 500,
+  reconnectionDelayMax: 2000,
 });
+
