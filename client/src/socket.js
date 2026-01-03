877dc9510eeecd830de8223cce71577e7f29859c
arturz777 <margo310507@gmail.com>
2026-01-03 12:10:20 +0200

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,11 +1,8 @@
 import { io } from "socket.io-client";
 
 export const socket = io(process.env.REACT_APP_API_URL, {
+  transports: ["websocket"],
   withCredentials: true,
-  transports: ["websocket", "polling"],
+  autoConnect: true,
 });
-
-socket.on("connect", () => console.log("✅ socket connected", socket.id));
-socket.on("disconnect", (r) => console.log("❌ socket disconnected", r));
-socket.on("connect_error", (e) => console.log("🚫 connect_error", e.message));
 
