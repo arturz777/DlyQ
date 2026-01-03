cf9e9b2d4bc77525b61d86afb75d65b17b677dbc
arturz777 <margo310507@gmail.com>
2026-01-03 20:09:15 +0200

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -7,15 +7,3 @@ export const socket = io(socketUrl, {
   withCredentials: true,
   autoConnect: true,
 });
-
-socket.on("connect", () => {
-  console.log("✅ socket connected:", socket.id);
-});
-
-socket.on("disconnect", (reason) => {
-  console.log("❌ socket disconnect:", reason);
-});
-
-socket.on("connect_error", (err) => {
-  console.log("🔥 socket connect_error:", err.message);
-});
