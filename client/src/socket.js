41cdf1b3e022eb76628d0068ba6b4000301bbab5
arturz777 <margo310507@gmail.com>
2026-01-03 20:21:56 +0200

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.REACT_APP_API_URL, {
+const raw = process.env.REACT_APP_API_URL || "";
+const socketUrl = raw.replace(/\/+$/, "").replace(/\/api$/, "");
+export const socket = io(socketUrl, {
   transports: ["websocket"],
   withCredentials: true,
   autoConnect: true,
