648ffc61b54ffa6836280a3c5e8e2ea8fe8b4710
arturz777 <margo310507@gmail.com>
2026-01-03 20:18:44 +0200

    Update index.js

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -1,17 +1,17 @@
 const chatSocket = require("./chatSocket");
 const orderSocket = require("./orderSocket");
 const warehouseSocket = require("./warehouseSocket");
+const userSocket = require("./userSocket");
 
 module.exports = function registerSockets(io) {
   io.on("connection", (socket) => {
-    console.log("🔌 socket connected:", socket.id);
 
+    userSocket(io, socket);
     chatSocket(io, socket);
     orderSocket(io, socket);
     warehouseSocket(io, socket);
 
     socket.on("disconnect", () => {
-      console.log("❌ socket disconnected:", socket.id);
     });
   });
 };
