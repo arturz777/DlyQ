1a470d153b0d1ec06ce92a363f63d5407a8ef798
arturz777 <margo310507@gmail.com>
2026-01-12 00:17:19 +0200

    Update chatRouter.js

--- server/routes/chatRouter.js
+++ server/routes/chatRouter.js
@@ -3,7 +3,22 @@ const router = new Router();
 const chatController = require("../controllers/chatController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/delivery/:orderId", authMiddleware, chatController.getOrCreateDeliveryChat);
+router.get(
+  "/delivery/:orderId",
+  authMiddleware,
+  chatController.getOrCreateDeliveryChat
+);
+router.get("/support", authMiddleware, chatController.getOrCreateSupportChat);
+router.get(
+  "/seller/:orderId",
+  authMiddleware,
+  chatController.getOrCreateSellerChat
+);
+router.get(
+  "/support/me",
+  authMiddleware,
+  chatController.getOrCreateSupportChat
+);
 
 router.get("/user/:userId", chatController.getUserChats);
 router.post("/", chatController.createChat);
