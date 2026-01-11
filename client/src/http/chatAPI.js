f31360944b89750d69c247157deb1bdee364eb1f
arturz777 <margo310507@gmail.com>
2026-01-12 00:26:24 +0200

    Update chatAPI.js

--- client/src/http/chatAPI.js
+++ client/src/http/chatAPI.js
@@ -1,6 +1,11 @@
-import { $authHost } from './index';
+import { $authHost } from "./index";
 
 export const fetchDeliveryChat = async (orderId) => {
   const { data } = await $authHost.get(`/chat/delivery/${orderId}`);
+  return data;
+};
+
+export const fetchSellerChat = async (orderId) => {
+  const { data } = await $authHost.get(`/chat/seller/${orderId}`);
   return data;
 };
