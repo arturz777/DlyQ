1fc61ce0e54d30674c08f257650bc4139bb0cb8f
arturz777 <margo310507@gmail.com>
2026-01-12 00:25:53 +0200

    Update ChatContext.js

--- client/src/context/ChatContext.js
+++ client/src/context/ChatContext.js
@@ -1,8 +1,9 @@
 import { createContext } from "react";
 
 export const ChatContext = createContext({
-  supportChatVisible: false,
-  supportChatId: null,
-  openSupportChat: () => {},
-  closeSupportChat: () => {},
+  chatVisible: false,
+  chatId: null,
+  chatMode: support | delivery | restaurant,
+  openChat: (chatId, mode) => {},
+  closeChat: () => {},
 });
