db3d59de51956fa65517825e935f797e417c7238
arturz777 <margo310507@gmail.com>
2026-01-15 12:19:15 +0200

    Update ChatModal.js

--- client/src/components/modals/ChatModal.js
+++ client/src/components/modals/ChatModal.js
@@ -7,10 +7,10 @@ import { useTranslation } from "react-i18next";
 import styles from "./ChatModal.module.css";
 
 const ChatModal = () => {
-  const { chatVisible, chatId, chatMode, closeChat } = useContext(ChatContext);
+  const { chatVisible, chatId, chatMode, closeChat } =
+    useContext(ChatContext);
   const { user } = useContext(Context);
-    const { t, i18n } = useTranslation();
-
+  const { t, i18n } = useTranslation();
 
   if (!chatVisible) return null;
 
@@ -18,22 +18,39 @@ const ChatModal = () => {
 
   return (
     <div className={styles.modalWrapper} onClick={closeChat}>
-      <div className={styles.chatContainer} onClick={(e) => e.stopPropagation()}>
+      <div
+        className={styles.chatContainer}
+        onClick={(e) => e.stopPropagation()}
+      >
         {!isAuthed ? (
           <div className={styles.guestBox}>
-            <div className={styles.guestTitle}>{t("you are not registered", { ns: "auth" })}</div>
+            <div className={styles.guestTitle}>
+              {t("you are not registered", { ns: "auth" })}
+            </div>
             <div className={styles.guestText}>
               {t("log in or sign up to contact support", { ns: "auth" })}
             </div>
 
             <div className={styles.guestActions}>
-              <Link to="/login" className={styles.guestBtnPrimary} onClick={closeChat}>
+              <Link
+                to="/login"
+                className={styles.guestBtnPrimary}
+                onClick={closeChat}
+              >
                 {t("login", { ns: "auth" })}
               </Link>
-              <Link to="/registration" className={styles.guestBtn} onClick={closeChat}>
+              <Link
+                to="/registration"
+                className={styles.guestBtn}
+                onClick={closeChat}
+              >
                 {t("registration", { ns: "auth" })}
               </Link>
-              <button className={styles.guestBtn} onClick={closeChat} type="button">
+              <button
+                className={styles.guestBtn}
+                onClick={closeChat}
+                type="button"
+              >
                 {t("close", { ns: "auth" })}
               </button>
             </div>
