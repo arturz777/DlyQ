6e169d455312c5281f1b2c1c30313dd77a242c41
arturz777 <margo310507@gmail.com>
2026-01-16 23:37:53 +0200

    Update chatAPI.js

--- client/src/http/chatAPI.js
+++ client/src/http/chatAPI.js
@@ -1,36 +1,36 @@
 import { $authHost } from "./index";
 
 export const fetchUserChats = async (userId) => {
-  const { data } = await $authHost.get(`/api/chat/user/${userId}`);
+  const { data } = await $authHost.get(`/chat/user/${userId}`);
   return data;
 };
 
 export const fetchChatById = async (chatId) => {
-  const { data } = await $authHost.get(`/api/chat/${chatId}`);
+  const { data } = await $authHost.get(`/chat/${chatId}`);
   return data;
 };
 
 export const fetchChatMessages = async (chatId) => {
-  const { data } = await $authHost.get(`/api/chat/${chatId}/messages`);
+  const { data } = await $authHost.get(`/chat/${chatId}/messages`);
   return data;
 };
 
 export const markChatRead = async (chatId, userId) => {
-  const { data } = await $authHost.put(`/api/chat/${chatId}/mark-read`, { userId });
+  const { data } = await $authHost.put(`/chat/${chatId}/mark-read`, { userId });
   return data;
 };
 
 export const openSupportChat = async () => {
-  const { data } = await $authHost.get(`/api/chat/support`);
+  const { data } = await $authHost.get(`/chat/support`);
   return data;
 };
 
 export const fetchDeliveryChat = async (orderId) => {
-  const { data } = await $authHost.get(`/api/chat/delivery/${orderId}`);
+  const { data } = await $authHost.get(`/chat/delivery/${orderId}`);
   return data;
 };
 
 export const fetchSellerChat = async (orderId) => {
-  const { data } = await $authHost.get(`/api/chat/seller/${orderId}`);
+  const { data } = await $authHost.get(`/chat/seller/${orderId}`);
   return data;
 };
