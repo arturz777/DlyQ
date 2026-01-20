c4d52e6bae817b1df97074b3b29185add1f51631
arturz777 <margo310507@gmail.com>
2026-01-20 19:00:32 +0200

    Update menuOptionsAPI.js

--- client/src/http/menuOptionsAPI.js
+++ client/src/http/menuOptionsAPI.js
@@ -1,36 +1,36 @@
 import { $authHost, $host } from "./index";
 
 export const fetchMenuItemOptions = async (itemId) => {
-  const { data } = await $host.get(`/api/menu/item/${itemId}/options`);
+  const { data } = await $host.get(`/menu/item/${itemId}/options`);
   return data;
 };
 
 export const createOptionGroup = async (payload) => {
-  const { data } = await $authHost.post(`/api/menu/option-groups`, payload);
+  const { data } = await $authHost.post(`/menu/option-groups`, payload);
   return data;
 };
 
 export const updateOptionGroup = async (id, payload) => {
-  const { data } = await $authHost.put(`/api/menu/option-groups/${id}`, payload);
+  const { data } = await $authHost.put(`/menu/option-groups/${id}`, payload);
   return data;
 };
 
 export const deactivateOptionGroup = async (id) => {
-  const { data } = await $authHost.post(`/api/menu/option-groups/${id}/deactivate`);
+  const { data } = await $authHost.post(`/menu/option-groups/${id}/deactivate`);
   return data;
 };
 
 export const createOption = async (payload) => {
-  const { data } = await $authHost.post(`/api/menu/options`, payload);
+  const { data } = await $authHost.post(`/menu/options`, payload);
   return data;
 };
 
 export const updateOption = async (id, payload) => {
-  const { data } = await $authHost.put(`/api/menu/options/${id}`, payload);
+  const { data } = await $authHost.put(`/menu/options/${id}`, payload);
   return data;
 };
 
 export const deactivateOption = async (id) => {
-  const { data } = await $authHost.post(`/api/menu/options/${id}/deactivate`);
+  const { data } = await $authHost.post(`/menu/options/${id}/deactivate`);
   return data;
 };
