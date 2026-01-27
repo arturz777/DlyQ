656d672f15da79233730a08166746442c5d748b2
arturz777 <margo310507@gmail.com>
2026-01-27 20:04:04 +0200

    Create paymentsAPI.js

---
+++ client/src/http/paymentsAPI.js
@@ -0,0 +1,26 @@
+import { $authHost } from "./index";
+
+export const fetchPaymentMethods = async () => {
+  const { data } = await $authHost.get("/payments/payment-methods");
+  return data;
+};
+
+export const detachPaymentMethod = async (pmId) => {
+  const { data } = await $authHost.post("/payments/detach-pm", { pmId });
+  return data;
+};
+
+export const createPaymentIntent = async (payload) => {
+  const { data } = await $authHost.post("/payments/create-intent", payload);
+  return data;
+};
+
+export const createSetupIntent = async (payload) => {
+  const { data } = await $authHost.post("/payments/setup-intent", payload);
+  return data;
+};
+
+export const setDefaultPaymentMethod = async (pmId) => {
+  const { data } = await $authHost.post("/payments/set-default", { pmId });
+  return data;
+};
