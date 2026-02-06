0ecc687b0d1b54ba4287b9ee4b7a8d1d96ce8d82
arturz777 <margo310507@gmail.com>
2026-02-07 01:19:37 +0200

    Update accountingAPI.js

--- client/src/http/accountingAPI.js
+++ client/src/http/accountingAPI.js
@@ -1,9 +1,25 @@
 import { $authHost } from "./index";
 
-export const fetchCourierAccounting = async ({ from, to }) => {
-  const { data } = await $authHost.get("/accounting/couriers", {
-    params: { from, to },
-  });
+export const fetchCourierAccounting = async (params) => {
+  const { data } = await $authHost.get("/accounting/couriers", { params });
+  return data;
+};
+
+export const fetchIncomeShop = async (params) => {
+  const { data } = await $authHost.get("/accounting/income/shop", { params });
+  return data;
+};
+
+export const fetchIncomeSellers = async (params) => {
+  const { data } = await $authHost.get("/accounting/income/sellers", { params });
+  return data;
+};
+
+export const fetchCourierIncomeOrders = async (courierId, params) => {
+  const { data } = await $authHost.get(
+    `/accounting/income/couriers/${courierId}/orders`,
+    { params }
+  );
   return data;
 };
 
