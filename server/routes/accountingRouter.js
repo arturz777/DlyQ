6e4a67281fce224dc13c71e95f5f7b995ba02c0e
arturz777 <margo310507@gmail.com>
2026-02-07 01:21:56 +0200

    Update accountingRouter.js

--- server/routes/accountingRouter.js
+++ server/routes/accountingRouter.js
@@ -7,7 +7,28 @@ router.get(
   "/couriers",
   authMiddleware,
   checkRoleMiddleware("ADMIN"),
-  accountingController.getCourierAccounting
+  accountingController.getCourierAccounting,
+);
+
+router.get(
+  "/income/shop",
+  authMiddleware,
+  checkRoleMiddleware("ADMIN"),
+  accountingController.getIncomeShop,
+);
+
+router.get(
+  "/income/sellers",
+  authMiddleware,
+  checkRoleMiddleware("ADMIN"),
+  accountingController.getIncomeSellers,
+);
+
+router.get(
+  "/income/couriers/:courierId/orders",
+  authMiddleware,
+  checkRoleMiddleware("ADMIN"),
+  accountingController.getCourierIncomeOrders,
 );
 
 module.exports = router;
