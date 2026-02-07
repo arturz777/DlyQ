db47c074f01c79d277205f763de884d2ffcb246c
arturz777 <margo310507@gmail.com>
2026-02-07 23:42:19 +0200

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,30 +1,42 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/models");
 
-module.exports = function (req, res, next) {
-    if (req.method === "OPTIONS") {
-        return next();
+module.exports = async function (req, res, next) {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
+  try {
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader) {
+      req.user = null;
+      return next();
     }
-    try {
-        const authorizationHeader = req.headers.authorization;
 
-        if (!authorizationHeader) {
-            req.user = null; 
-            return next(); 
-        }
+    const token = authorizationHeader.split(" ")[1];
 
-        const token = authorizationHeader.split(' ')[1]; 
-
-        if (!token) {
-            req.user = null;
-            return next(); 
-        }
-
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = decoded;
-        return next();
-
-    } catch (error) {
-        req.user = null; 
-        return next();
+    if (!token) {
+      req.user = null;
+      return next();
     }
+
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+    const user = await User.findByPk(decoded.id, { attributes: ["id", "isBlocked"] });
+    if (!user) {
+      req.user = null;
+      return next();
+    }
+
+    if (user.isBlocked) {
+      return res.status(403).json({ message: "Пользователь заблокирован" });
+    }
+
+    req.user = decoded;
+    return next();
+  } catch (error) {
+    req.user = null;
+    return next();
+  }
 };
