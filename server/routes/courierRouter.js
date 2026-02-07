e3f1bb40e4cb6dff85dd4d909cfbba763154e018
arturz777 <margo310507@gmail.com>
2026-02-08 00:02:35 +0200

    Update courierRouter.js

--- server/routes/courierRouter.js
+++ server/routes/courierRouter.js
@@ -8,105 +8,141 @@ router.get(
   "/orders",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.getActiveOrders
+  courierController.getActiveOrders,
 );
-
 router.post(
   "/orders/:id/accept",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.acceptOrder
+  courierController.acceptOrder,
 );
 
 router.get(
   "/orders/:id/self-pick-info",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.selfPickInfo
+  courierController.selfPickInfo,
 );
-
 router.post(
   "/orders/:id/self-pick",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.selfPick
+  courierController.selfPick,
 );
 
 router.get(
   "/self-pick-candidates",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.selfPickCandidates
+  courierController.selfPickCandidates,
 );
 
 router.post(
   "/status",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.toggleCourierStatus
+  courierController.toggleCourierStatus,
 );
 
 router.post(
   "/orders/:id/complete",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.completeDelivery
+  courierController.completeDelivery,
 );
-
 router.post(
   "/orders/:id/status",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.updateDeliveryStatus
+  courierController.updateDeliveryStatus,
 );
 
 router.post(
   "/update-location",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.updateCourierLocation
+  courierController.updateCourierLocation,
 );
 
 router.get(
   "/me",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.getSelf
+  courierController.getSelf,
 );
-
 router.post(
   "/push-token",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.savePushToken
+  courierController.savePushToken,
 );
 
 router.post(
   "/orders/:id/decline",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.declineOrder
-);
-
-router.get(
-  "/couriers",
-  authMiddleware,
-  checkRoleMiddleware("ADMIN"),
-  courierController.getAllCouriers
+  courierController.declineOrder,
 );
 
 router.get(
   "/history",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.getHistory
+  courierController.getHistory,
 );
-
 router.get(
   "/finance",
   authMiddleware,
   roleMiddleware("COURIER"),
-  courierController.getFinance
+  courierController.getFinance,
+);
+
+router.get(
+  "/couriers",
+  authMiddleware,
+  checkRoleMiddleware("ADMIN"),
+  courierController.getAllCouriers,
+);
+
+router.get(
+  "/admin/users",
+  authMiddleware,
+  checkRoleMiddleware("ADMIN"),
+  courierController.adminSearchUsers,
+);
+
+router.post(
+  "/admin/:userId/make",
+  authMiddleware,
+  checkRoleMiddleware("ADMIN"),
+  courierController.adminMakeCourier,
+);
+router.post(
+  "/admin/:userId/remove",
+  authMiddleware,
+  checkRoleMiddleware("ADMIN"),
+  courierController.adminRemoveCourier,
+);
+
+router.patch(
+  "/admin/:userId",
+  authMiddleware,
+  checkRoleMiddleware("ADMIN"),
+  courierController.adminUpdateCourierProfile,
+);
+
+router.post(
+  "/admin/:userId/reset-push",
+  authMiddleware,
+  checkRoleMiddleware("ADMIN"),
+  courierController.adminResetCourierPushToken,
+);
+
+router.post(
+  "/admin/:userId/block",
+  authMiddleware,
+  checkRoleMiddleware("ADMIN"),
+  courierController.adminToggleUserBlock,
 );
 
 module.exports = router;
