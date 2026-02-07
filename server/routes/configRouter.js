d4baea37a32f54d30437c825e889d13ed430f2c0
arturz777 <margo310507@gmail.com>
2026-02-08 00:03:35 +0200

    Update configRouter.js

--- server/routes/configRouter.js
+++ server/routes/configRouter.js
@@ -1,10 +1,13 @@
 const Router = require("express");
 const router = new Router();
+
 const {
   getMaintenance,
   setMaintenance,
   getShopConfig,
   setShopConfig,
+  getDeliveryPricing,
+  setDeliveryPricing,
 } = require("../controllers/configController");
 
 router.get("/maintenance", getMaintenance);
@@ -12,5 +15,8 @@ router.post("/maintenance", setMaintenance);
 
 router.get("/shop", getShopConfig);
 router.post("/shop", setShopConfig);
+
+router.get("/delivery", getDeliveryPricing);
+router.post("/delivery", setDeliveryPricing);
 
 module.exports = router;
