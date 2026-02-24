72af6989f7b66b5da97cf05ac645c79979a017c3
arturz777 <margo310507@gmail.com>
2026-02-24 16:35:44 +0200

    Update index.js

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -2,20 +2,14 @@ const chatSocket = require("./chatSocket");
 const orderSocket = require("./orderSocket");
 const warehouseSocket = require("./warehouseSocket");
 const userSocket = require("./userSocket");
+const courierSocket = require("./courierSocket");
 
 module.exports = function registerSockets(io) {
   io.on("connection", (socket) => {
-
-     socket.on("joinCourierRoom", ({ courierId }) => {
-      if (courierId) socket.join(`courier:${courierId}`);
-    });
-
+    courierSocket(io, socket);
     userSocket(io, socket);
     chatSocket(io, socket);
     orderSocket(io, socket);
     warehouseSocket(io, socket);
-
-    socket.on("disconnect", () => {
-    });
   });
 };
